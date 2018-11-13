e69bcb315a532388dfc3b5600caf127cd58d8221
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-11-13 21:31:55 +0200

    Update biscuits.pl

--- biscuits.pl
+++ biscuits.pl
@@ -1,11 +1,11 @@
-% --------            NOOB VERSION            --------% 
+% --------            NOOB VERSION            -------- % 
 
 add([Result],0,Y,X,Result). 	 
 add([List],N,Y,X,Z) :- N>1, C is N-1, Num is Y-2, Rem is X-Y, append(List,[Num],Result), add([Result],C,Num,Rem,Z).
 add([List],N,Y,X,Z) :- N=<1, C is N-1, Num is Y-2, Rem is X-Y, append(List,[Rem],Result), add([Result],C,Num,Rem,Z).
 biscuits(X,Y,Z) :- N is X/Y, C is Y-1, append([],[N],L), add([L],C,N,X,Z).
 
-% --------            PRO VERSION            --------%
+% --------            PRO VERSION            -------- %
 
 biscuits(_,0,[]).
 biscuits(K,1,[K|L]) :- biscuits(K,0,L).
