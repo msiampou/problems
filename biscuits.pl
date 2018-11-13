585a7af362ce113aa6b0c78429e0459b1ed3c9e5
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-11-13 17:06:23 +0200

    Update biscuits.pl

--- biscuits.pl
+++ biscuits.pl
@@ -1,4 +1,4 @@
-add([Result],0,Y,X,Result). % base case
-add([List],N,Y,X,Z) :- N>1, C is N-1, Num is Y-1, Rem is X-Y, append(List,[Num],Result), add([Result],C,Num,Rem,Z).
-add([List],N,Y,X,Z) :- N=<1, C is N-1, Num is Y-1, Rem is X-Y, append(List,[Rem],Result), add([Result],C,Num,Rem,Z).
+add([Result],0,Y,X,Result). 	 
+add([List],N,Y,X,Z) :- N>1, C is N-1, Num is Y-2, Rem is X-Y, append(List,[Num],Result), add([Result],C,Num,Rem,Z).
+add([List],N,Y,X,Z) :- N=<1, C is N-1, Num is Y-2, Rem is X-Y, append(List,[Rem],Result), add([Result],C,Num,Rem,Z).
 biscuits(X,Y,Z) :- N is X/Y, C is Y-1, append([],[N],L), add([L],C,N,X,Z).
