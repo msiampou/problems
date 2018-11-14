164d525e6e71915d1ccdfcd4110373d34d144d88
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-11-14 19:09:18 +0200

    Update solvelists.pl

--- solvelists.pl
+++ solvelists.pl
@@ -1,9 +1,13 @@
-replace(_, _, [], []).
-replace(X, R, [X|T1], [R|T2]) :- replace(X, R, T1, T2).
-replace(X, R, [H|T1], [H|T2]) :- \+ (H = X), replace(X, R, T1, T2).
+findU([],_, U, U).
+findU(_,[], U, U).
+findU([H1|L1], [H2|L2], U, Res) :- Diff is H1-H2, abs(Diff,Num), Num > U, findU(L1, L2, Num, Res).
+findU([H1|L1], [H2|L2], U, Res) :- Diff is H1-H2, abs(Diff,Num), Num =< U, findU(L1, L2, U, Res).
 
-solvelists([],[],[]).
-solvelists([H1|T1], [H2|T2], [(H2,H1)|NewT]) :- number(H1), \+ number(H2), replace(H2,H1,T2,L2), replace(H2,H1,T1,L1), solvelists(L1, L2, NewT).
-solvelists([H1|T1], [H2|T2], [(H1,H2)|NewT]) :- \+ number(H1), number(H2), replace(H1,H2,T1,L1), replace(H1,H2,T2,L2) ,solvelists(L1, L2, NewT).
-solvelists([H1|T1], [H2|T2], NewT) :- \+ number(H1), \+ number(H2), H1 == H2, solvelists(T1, T2, NewT).
-solvelists([H1|T1], [H2|T2], NewT) :- number(H1), number(H2), H1 =:= H2, solvelists(T1, T2, NewT).
+lessUgly([], _, _, _, N, N).
+lessUgly(_, [], _, _, N, N).
+lessUgly([H1|L1], [H2|L2], [H3|L3], U, _, Res) :- Diff is H1-H2, abs(Diff,Num), Num > U, lessUgly(L1, L2, L3, Num, H3, Res).
+lessUgly([H1|L1], [H2|L2], [_|L3], U, N, Res) :- Diff is H1-H2, abs(Diff,Num), Num =< U, lessUgly(L1, L2, L3, U, N, Res).
+
+ugliness(L1,L2,U) :- sort(L1, NewL1),sort(L2, NewL2),length(L1,Len1),length(L2,Len2),Len1 =:= Len2,findU(NewL1,NewL2,0, U).
+ugliness(L1,L2,U) :- sort(L1, NewL1),sort(L2, NewL2),length(L1,Len1),length(L2,Len2),Len1 > Len2,lessUgly(NewL1,NewL2,NewL1,0,0,Pos),delete(NewL1,Pos,NewList3),findU(NewList3,NewL2,0, U).
+ugliness(L1,L2,U) :- sort(L1, NewL1),sort(L2, NewL2),length(L1,Len1),length(L2,Len2),Len1 < Len2,lessUgly(NewL1,NewL2,NewL2,0,0,Pos),delete(NewL2,Pos,NewList3),findU(NewL1,NewList3,0,U).
