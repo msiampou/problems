11a10ccc39080c099331b719d3005c86f65f3945
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-11-29 22:46:26 +0200

    Delete sum.pl

--- sum.pl
+++
@@ -1,4 +0,0 @@
-list_sum([],Move).
-list_sum([Sum|[]],Move) :- Sum>10, Move = "next", list_sum([],Move).
-list_sum([Sum|[]],Move) :- Sum=<10, Move = "stop", list_sum([],Move).
-list_sum([H1,H2|T],Move) :- N is H1+H2, list_sum([N|T],Move).
