f0469ddf80450692491751642e2aaaedc7d99784
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-12-05 11:46:19 +0200

    Update all.pl

--- all.pl
+++ all.pl
@@ -66,8 +66,8 @@ dif([H|T],Num,[D1|L]) :- D is H-Num, abs(D,D1), dif(T,Num,L).
 fix([],_,[]).
 fix([H|L1],L2,[Num|R]) :- dif(L2,H,Diff), min_list(Diff,Min), indexOf(Diff,Min,Pos), nth0(Pos,L2,Num), select(Num,L2,L3), fix(L1,L3,R).
 
-ugliness(X,Y,U) :- msort(X,L1), msort(Y,L2), length(L1,Len1), length(L2,Len2), Len1 > Len2, fix(L2,L1,F), findU(F,L2,0,U).
-ugliness(X,Y,U) :- msort(X,L1), msort(Y,L2), length(L1,Len1), length(L2,Len2), Len1 < Len2, fix(L1,L2,F), findU(L1,F,0,U).
+ugliness(X,Y,U) :- msort(X,L1), msort(Y,L2), length(L1,Len1), length(L2,Len2), Len1 > Len2, fix(L2,L1,F), msort(F,F1), findU(F1,L2,0,U).
+ugliness(X,Y,U) :- msort(X,L1), msort(Y,L2), length(L1,Len1), length(L2,Len2), Len1 < Len2, fix(L1,L2,F), msort(F,F1), findU(L1,F1,0,U).
 ugliness(X,Y,U) :- msort(X,L1), msort(Y,L2), findU(L1,L2,0,U).
 
 %----------------------------------------- Ex.4 ------------------------------------------------------------------------------------------------------%
