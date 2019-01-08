40aa69fa614db5b098a3c115e5a7a3870a31c38e
Maria Despoina Siampou <sdi1600151@di.uoa.gr>
2019-01-08 03:54:40 +0200

    Delete multiples.hs

--- multiples.hs
+++
@@ -1,8 +0,0 @@
-listsort [] ys = ys
-listsort xs [] = xs
-listsort (x:xs) (y:ys)
-                       | x < y = x : listsort xs (y:ys)
-                       | x > y = y : listsort (x:xs) ys
-                       | x == y = x : listsort xs ys
-
-multiples = 1 : listsort(map (2*) multiples) (listsort (map (3*) multiples) (map(5*) multiples))
