403e2aa7e6fda0d3e5ad36b57840f669418e6593
Maria Despoina Siampou <siampoumaria@gmail.com>
2019-02-23 17:14:55 +0200

    Update bfs.hs

--- Haskell/PancakesSortingProblem/bfs.hs
+++ Haskell/PancakesSortingProblem/bfs.hs
@@ -1,3 +1,9 @@
+import Data.List
+import Debug.Trace
+import Functions
+import Data.Function (on)
+import qualified Data.Set as Set
+
 ------------------------------
 ---- BSF Sorting Function
 ------------------------------
